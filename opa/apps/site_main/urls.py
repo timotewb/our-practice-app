70e404f04b10b28b07ac714a76d1a5ce9c98ef05
timotewb <timote.wb@gmail.com>
2023-03-23 06:59:03 +1300

    register

--- opa/apps/site_main/urls.py
+++ opa/apps/site_main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "sm"
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register', views.register_request, name='register'),
 ]
